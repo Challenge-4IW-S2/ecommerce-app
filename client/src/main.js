7aebdbd3fc38412de44dbdc2056ea7cd445684a1
Estelle <estelle272001@gmail.com>
2024-06-02 23:24:05 +0200

    DB connection

--- client/src/main.js
+++ client/src/main.js
@@ -3,3 +3,23 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+server.get('/', async (req, res) => {
+    try {
+        const data =  await pool.query('SELECT * FROM users');
+        res.status(200).send(data.rows );
+    } catch (err) {
+        console.error(err.message);
+        res.sendStatus(500);
+    }
+});
+
+server.post('/',async (req, res) => {
+    try {
+        await pool.query('INSERT INTO users (name, location) VALUES ($1, $2)', ['brianc', 'San Francisco']);
+        res.status(200).send({ message: 'User added.' });
+    } catch (err) {
+        console.error(err.message);
+        res.sendStatus(500);
+    }
+})
+
