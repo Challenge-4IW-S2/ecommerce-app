0dd3d3771bf86804cabb223a11ac03ce3f37f1b6
ines-mgg <i.maganga@yahoo.com>
2024-06-04 21:51:40 +0200

    update

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,4 @@
- import express from "express";
+import express from "express";
 import { indexRouter } from "./routes/index.js";
 import "./mongo/mongodb.js";
 import "./postgresql/postgresql.js";
@@ -7,18 +7,18 @@ import "./postgresql/postgresql.js";
 const server = express();
 const port = 8000;
 server.use("/", indexRouter);
-server.use(express.json());
- server.get('/setup', async (req, res) => {
-     try {
-         await pool.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(100), location TEXT)');
-         res.status(200).send({ message: 'Table Created' });
+// server.use(express.json());
+// server.get('/setup', async (req, res) => {
+//      try {
+//          await pool.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(100), location TEXT)');
+//          res.status(200).send({ message: 'Table Created' });
 
-     } catch (err) {
-         console.error(err.message);
-         res.sendStatus(500);
-     }
- });
+//      } catch (err) {
+//          console.error(err.message);
+//          res.sendStatus(500);
+//      }
+// });
 
-server.listen(8000, "0.0.0.0", () => {
+server.listen(port, "0.0.0.0", () => {
   console.log("Server listening on http://localhost:8000");
 });
