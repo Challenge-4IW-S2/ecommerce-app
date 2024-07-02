c27a0a1055359608338f13278c6ad37be349ed22
Estelle <72598682+Stelleu@users.noreply.github.com>
2024-07-02 09:25:31 +0200

    Update UtilitiesController.js
    add SendMail function

--- server/src/controllers/UtilitiesController.js
+++ server/src/controllers/UtilitiesController.js
@@ -20,4 +20,37 @@ export class UtilitiesController {
             res.status(404).json({ message: `Model ${modelName} not found` });
         }
     }
+
+    static async sendMail(request,response){
+         const parameters = {
+            email: request.body.email,
+            name: request.body.firstname,
+            subject: request.body.subject,
+            template: request.body.template,
+            message: request.body.message
+            }
+        try{
+            if (
+            		!email || !email.trim() ||
+            		!name || !name.trim() ||
+            		!subject || !subject.trim() ||
+            		!message || !message.trim()
+	            ) {
+		            return res.status(400).json({ message: 'name, email, subject and message are required' });
+	              }
+
+	const receipients = ` ${name} <${email}>`;
+
+	res.json({ message: 'Sending email in a moment...' });
+
+	sendEmail({ parameters.email, parameters.subject, parameters.message, parameters.template })
+		.then(result => { })
+		.catch(error => {
+			// console.log(`Unable to send email to ${JSON.stringify({ receipients })}: ${JSON.stringify(error)}`)
+		});
+            
+        }catch(err){
+            
+        }
+    }
 }
