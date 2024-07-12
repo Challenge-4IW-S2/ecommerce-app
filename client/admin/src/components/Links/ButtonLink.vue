4911132082866b4368151663801904f4adfcb0be
Bilel SAIGH <bilelsaigh94@gmail.com>
2024-07-12 02:17:16 +0200

    Debut de la création de la 2eme app pour le BOffice

---
+++ client/admin/src/components/Links/ButtonLink.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+const sizingStyle = ` block text-center content-center`;
+
+defineProps({
+  to: {
+    type: String,
+    default: "#"
+  },
+  text: {
+    type: String,
+    default: 'Action'
+  },
+  className: {
+    type: String,
+    default: "bg-black text-white"
+  }
+})
+</script>
+
+<template>
+  <RouterLink :to="to" :class="className + sizingStyle">
+    {{ text }}
+  </RouterLink>
+</template>
+
+<style scoped>
+
+</style>
