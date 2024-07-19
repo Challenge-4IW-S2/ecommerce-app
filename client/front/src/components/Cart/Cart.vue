548ff81a6196282651f9d31b7f2e9bdaa7a15c88
Bilel SAIGH <bilelsaigh94@gmail.com>
2024-07-19 03:16:02 +0200

    add store pinia and cart state

--- client/front/src/components/Cart/Cart.vue
+++ client/front/src/components/Cart/Cart.vue
@@ -1,22 +1,17 @@
 <script setup>
-import { ref } from 'vue';
-import OpenCart from './OpenCart.vue';
-const isCartOpen = ref(false);
-const openCart = () => {
-    isCartOpen.value = !isCartOpen.value;
-}
+import { useCartStore } from '../../store/cart.js'
+import OpenCart from "./OpenCart.vue"
 
-const productNumber = ref(0);
+const cartStore = useCartStore()
 </script>
 
 <template>
-    <div v-if="isCartOpen" class="fixed inset-0 bg-black bg-opacity-50 z-10"></div>
+  <div v-if="cartStore.isCartOpen" class="fixed inset-0 bg-black bg-opacity-50 z-10"></div>
 
-    <button class="uppercase text-sm" @click="openCart">
-        Panier ({{ productNumber }})
-    </button>
-    <div v-if="isCartOpen">
-        <OpenCart :closeFunction=openCart />
-    </div>
-
+  <button class="uppercase text-sm" @click="cartStore.toggleCart">
+    Panier ({{ cartStore.itemCount }})
+  </button>
+  <div v-if="cartStore.isCartOpen">
+    <OpenCart :closeFunction="cartStore.toggleCart" />
+  </div>
 </template>
