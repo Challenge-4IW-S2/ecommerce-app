bd906ae9a2dd214e12f00f1af25b39b08be92a83
Bilel SAIGH <bilelsaigh94@gmail.com>
2024-07-20 03:54:37 +0200

    fix panier number

--- client/front/src/components/Cart/Cart.vue
+++ client/front/src/components/Cart/Cart.vue
@@ -1,15 +1,21 @@
 <script setup>
 import { useCartStore } from '../../store/cart.js'
 import OpenCart from "./OpenCart.vue"
+import {computed} from "vue";
 
 const cartStore = useCartStore()
+
+const totalQuantity = computed(() => {
+  return cartStore.items.reduce((total, item) => total + item.quantity, 0);
+});
+
 </script>
 
 <template>
   <div v-if="cartStore.isCartOpen" class="fixed inset-0 bg-black bg-opacity-50 z-10"></div>
 
   <button class="uppercase text-sm" @click="cartStore.toggleCart">
-    Panier ({{ cartStore.itemCount }})
+    Panier ({{ totalQuantity }})
   </button>
   <div v-if="cartStore.isCartOpen">
     <OpenCart :closeFunction="cartStore.toggleCart" />
