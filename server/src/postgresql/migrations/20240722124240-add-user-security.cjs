982cbf0c9c56e745e0b15d2b69226390934cccea
Estelle <estelle272001@gmail.com>
2024-07-22 17:51:23 +0200

    tentative de connexion

---
+++ server/src/postgresql/migrations/20240722124240-add-user-security.cjs
@@ -0,0 +1,30 @@
+'use strict';
+const {DataTypes} = require("sequelize");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('users','attempt_connexion',{
+      type: DataTypes.INTEGER,
+      allowNull: true
+    });
+    await queryInterface.addColumn('users','lock_until',{
+      type: DataTypes.DATE,
+      allowNull: true,
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
