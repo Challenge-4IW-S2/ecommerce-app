06b5a905760d3eb690c61459212b4472fa85709e
Bilel SAIGH <bilelsaigh94@gmail.com>
2024-07-23 12:13:01 +0200

    add webhook

--- server/src/controllers/StripeController.js
+++ server/src/controllers/StripeController.js
@@ -7,6 +7,12 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export const StripeController = {
     async createCheckoutSession(req, res) {
         try {
+
+            const metadata = items.reduce((acc, item) => {
+                acc[item.id] = item.quantity;
+                return acc;
+            }, {});
+
             const { items } = req.body; // Assurez-vous que les articles sont validés et nettoyés
             const session = await stripe.checkout.sessions.create({
                 payment_method_types: ['card'],
@@ -15,16 +21,17 @@ export const StripeController = {
                         currency: 'eur',
                         product_data: {
                             name: item.name,
+                            metadata: { productId: item.id } // Ajoutez l'ID du produit ici si nécessaire
                         },
                         unit_amount: item.price_ttc * 100,
                     },
                     quantity: item.quantity,
                 })),
+                metadata: metadata, // Ajoutez les métadonnées ici
                 mode: 'payment',
                 success_url: `http://localhost`,
                 cancel_url: `https://google.com`,
                 locale: 'fr',
-
             });
 
             console.log('Session de paiement créée avec succès:', session.id);
--- server/src/routes/payment.js
+++ server/src/routes/payment.js
@@ -1,9 +1,10 @@
-import express from 'express';
 import { StripeController } from '../controllers/StripeController.js';
+import { StripeWebhook } from '../controllers/StripeWebhook.js';
 import checkAuth from "../middlewares/checkAuth.js";
-export default function (router){
 
-    router.post('/create-payment-intent', checkAuth() ,StripeController.createCheckoutSession);
+export default function (router) {
+    router.post('/create-payment-intent', checkAuth(), StripeController.createCheckoutSession);
+    router.post('/stripe-webhook', checkAuth(), StripeWebhook.handleWebhook);
 
     return router;
 }
