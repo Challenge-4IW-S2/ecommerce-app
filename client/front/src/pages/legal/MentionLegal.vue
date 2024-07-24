cbdae06c90d77092ae8eb36095020b27a814dbfd
Estelle <estelle272001@gmail.com>
2024-07-24 13:37:56 +0200

    add seeders and legel folder

---
+++ client/front/src/pages/legal/MentionLegal.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+  <template>
+    <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16 text-sm">
+      <h1 class="text-center text-4xl  mb-4 pb-4">Mentions Légales</h1>
+      <p>Luzaya.fr est édité par la société Luzaya SARL, au capital de 10 000 euros, immatriculée au RCS de Paris sous le numéro 123 456 789, dont le siège social est situé au 245 Faubourg Saint Antoine, 75011 Paris.</p>
+
+      <h2 class="font-bold py-4 text-2xl">Éditeur du Site</h2>
+      <p>Nous collectons des informations lorsque vous vous inscrivez sur notre site, passez une commande, remplissez un formulaire ou naviguez sur notre site. Les informations collectées incluent votre nom, adresse e-mail, adresse postale et numéro de téléphone.</p>
+
+      <h2 class="font-bold py-4 text-2xl">Directeur de la Publication</h2>
+      <p>La directrice de la publication est Mme Nkumba Estelle, gérant de Luzaya SARL.</p>
+
+      <h2 class="font-bold py-4 text-2xl">Hébergement</h2>
+      <p>Le site Luzaya.fr est hébergé par Digital Ocean.</p>
+
+      <h2 class="font-bold py-4 text-2xl">Contact</h2>
+      <p>Pour toute question ou réclamation concernant le site, vous pouvez nous contacter par e-mail à luzaya@proton.me ou par téléphone au +33 6 52 14 41 89.</p>
+    </div>
+  </template>
+</template>
+
+<style scoped>
+
+</style>
+
+
+
+
+
+
+
+
